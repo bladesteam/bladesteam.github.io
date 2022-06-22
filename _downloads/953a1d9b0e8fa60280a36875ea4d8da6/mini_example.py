5292169b242f0c259ca85510cf2ae083791c3507
Shenghui Li <bladesteam.uu@gmail.com>
2022-06-22 20:27:40 +0200

    update

---
+++ _downloads/953a1d9b0e8fa60280a36875ea4d8da6/mini_example.py
@@ -0,0 +1,49 @@
+"""
+A mini example
+===========================
+
+"""
+
+import ray
+from blades.datasets import MNIST
+from blades.models.mnist import DNN
+from blades.simulator import Simulator
+
+
+
+# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"]="2,3"
+
+
+mnist = MNIST(data_root="./data", train_bs=32, num_clients=10)  # built-in federated MNIST dataset
+
+# configuration parameters
+conf_params = {
+    "dataset": mnist,
+    "aggregator": "mean",  # aggregation
+    "num_byzantine": 0,  # number of Byzantine input
+    "attack": "alie",  # attack strategy
+    "attack_params": {"num_clients": 1,  # attacker parameters
+                     "num_byzantine": 0},
+    "num_actors": 10,  # number of training actors
+    "use_cuda": True,
+    "gpu_per_actor": 0.19,
+    "seed": 1,  # reproducibility
+}
+
+ray.init(num_gpus=2, local_mode=False)
+simulator = Simulator(**conf_params)
+
+model = DNN()
+# runtime parameters
+run_params = {
+    "model": model,  # global model
+    "server_optimizer": 'SGD',  # ,server_opt  # server optimizer
+    "client_optimizer": 'SGD',  # client optimizer
+    "loss": "crossentropy",  # loss function
+    "global_rounds": 400,  # number of global rounds
+    "local_steps": 50,  # number of steps per round
+    "server_lr": 1.0,
+    "client_lr": 0.1,  # learning rate
+}
+simulator.run(**run_params)
---
+++ _downloads/ff1d7f85e6f9ad4796a3d83d46b69870/customize_attack.py
@@ -0,0 +1,54 @@
+"""
+Customization of attack strategy
+=================================
+
+"""
+
+
+import ray
+
+from blades.client import ByzantineClient
+from blades.simulator import Simulator
+from blades.datasets import MNIST
+from blades.models.mnist import DNN
+
+mnist = MNIST(data_root="./data", train_bs=32, num_clients=10)  # built-in federated MNIST dataset
+
+
+class MaliciousClient(ByzantineClient):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def omniscient_callback(self, simulator):
+        updates = []
+        for w in simulator.get_clients():
+            if not w.is_byzantine():
+                updates.append(w.get_update())
+        self.save_update(-100 * (sum(updates)) / len(updates))
+
+
+# configuration parameters
+conf_params = {
+    "dataset": mnist,
+    "aggregator": "mean",  # defense: robust aggregation
+    "num_actors": 4,  # number of training actors
+    "seed": 1,  # reproducibility
+}
+
+ray.init(num_gpus=0, local_mode=True)
+simulator = Simulator(**conf_params)
+
+attackers = [MaliciousClient() for _ in range(5)]
+simulator.register_attackers(attackers)
+
+# runtime parameters
+run_params = {
+    "model": DNN(),  # global model
+    "server_optimizer": 'SGD',  # server optimizer
+    "client_optimizer": 'SGD',  # client optimizer
+    "loss": "crossentropy",  # loss function
+    "global_rounds": 400,  # number of global rounds
+    "local_steps": 2,  # number of steps per round
+    "client_lr": 0.1,  # learning rate
+}
+simulator.run(**run_params)
